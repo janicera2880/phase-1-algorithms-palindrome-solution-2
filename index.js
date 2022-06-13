7ff930a76f828544e06652f679802501967941f5
Janice Alecha <janiceralecha@gmail.com>
2022-06-13 18:23:13 -0500

    commit tree 353a368336c711f700292b6d439fcbdfe48ea096

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+/*  First let's define what is Palindrome? It is a word, phrase, number, or other sequesnce of characters which reads the same backwards as forward such as racecar, mom or abba.
+I will need to make an isPalindrome function that returns either true or false if the string is palindrome or not, That means if the first letter is the same as the
+last letter, and the second letter is the same as the second to last letter, and so on the function should return true.
+*/
+
+// iterate from the beginning of the string to the middle of the string
+  for (let i = 0; i < word.length / 2; i++) {
+    // compare the letter we're iterating over to the corresponding letter at the end of the string
+    const j = word.length - 1 - i;
+    if (word[i] !== word[j]) {
+      // if the letters don't match, return false
+      return false;
+    }
+  }
+
+  // if we reach the middle, and all the letters match, return true
+  return true;
 }
 
+
+
 /* 
-  Add your pseudocode here
+  iterate from the beginning of the string to the middle of the string
+  compare the letter we're iterating over to the corresponding letter at the end of the string
+  if the letters don't match, return false
+  if we reach the middle, and all the letters match, return true
 */
 
 /*
-  Add written explanation of your solution here
+  Input: "racecar"
+Input length divided by two: 3.5
+Iteration:
+  Index 0 (less than 3.5, keep iterating)
+  Index 1 (less than 3.5, keep iterating)
+  Index 2 (less than 3.5, keep iterating)
+  Index 3 (less than 3.5, keep iterating)
+  Index 4 (not less than 3.5, stop iterating)
+For "racecar", our loop will iterate up to the middle "e"
+
+Input: "abba"
+Input length divided by two: 2
+Iteration:
+  Index 0 (less than 2, keep iterating)
+  Index 1 (less than 2, keep iterating)
+  Index 2 (not less than 2, stop iterating)
+For "abba", our loop will iterate up to the first "b"
 */
 
 // You can run `node index.js` to view these console logs
